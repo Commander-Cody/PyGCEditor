1e1a09d1a6a503d0c0f8f141c3b48122eb2b2d3c
Thore <thoreand@gmx.de>
2019-09-29 22:51:41 +0200

    Added more options when creating variant
    - coordinates and file
    - new planets still don't get saved

--- ui/planetvariantcreator.py
+++ ui/planetvariantcreator.py
@@ -19,6 +19,11 @@ class PlanetVariantCreator(Dialog):
         self.__inputBaseName: QLineEdit = QLineEdit(self.__dialog)
         self.__inputVariantName: QLineEdit = QLineEdit(self.__dialog)
 
+        self.__inputX: QLineEdit = QLineEdit(self.__dialog)
+        self.__inputY: QLineEdit = QLineEdit(self.__dialog)
+
+        self.__inputFile: QLineEdit = QLineEdit(self.__dialog)
+
         self.__okayButton: QPushButton = QPushButton("OK")
         self.__okayButton.clicked.connect(self.__okayClicked)
 
@@ -28,6 +33,11 @@ class PlanetVariantCreator(Dialog):
         self.__formLayout.addRow("New planet variant name", self.__inputVariantName)
         self.__formLayout.addRow("Base Planet", self.__inputBaseName)
 
+        self.__formLayout.addRow("X coordinate", self.__inputX)
+        self.__formLayout.addRow("Y coordinate", self.__inputY)
+
+        self.__formLayout.addRow("Add to file", self.__inputFile)
+        
         self.__buttonLayout.addWidget(self.__okayButton)
         self.__buttonLayout.addWidget(self.__cancelButton)
 
@@ -49,18 +59,21 @@ class PlanetVariantCreator(Dialog):
         '''Display dialog non-modally'''
         if basePlanet is not None:
             self.__inputBaseName.setText(basePlanet.name)
+            self.__inputX.setText(str(basePlanet.x))
+            self.__inputY.setText(str(basePlanet.y))
+            self.__inputFile.setText(basePlanet.containingFile)
         self.__setupAutoComplete()
         self.__dialog.exec()
         return self.__result
 
     def getCreatedPlanet(self) -> Planet:
         '''Returns the created planet object'''
-        base = self.__repository.getPlanetByName(self.__basePlanet)
         planet: Planet = Planet(self.__variantName)
 
-        planet.variantOf = base.name
-        planet.x = base.x
-        planet.y = base.y
+        planet.variantOf = self.__basePlanet
+        planet.x = self.__newX
+        planet.y = self.__newY
+        planet.containingFile = self.__file
         return planet
 
     def __setupAutoComplete(self) -> None:
@@ -68,18 +81,36 @@ class PlanetVariantCreator(Dialog):
         autoCompleter = AutoCompleter(self.__repository.getPlanetNames())
         planetCompleter = autoCompleter.completer()
         self.__inputBaseName.setCompleter(planetCompleter)
+        planetCompleter.activated.connect(self.__onCompletionSelected)
+        
+    def __onCompletionSelected(self, selectedPlanetName):
+        '''Fills out coordinate and file LineEdits with data from the selected planet'''
+        selectedPlanet = self.__repository.getPlanetByName(selectedPlanetName)
+        #if self.__inputX.text() == "":
+        self.__inputX.setText(str(selectedPlanet.x))
+        #if self.__inputY.text() == "":
+        self.__inputY.setText(str(selectedPlanet.y))
+        #if self.__inputFile.text() == "":
+        self.__inputFile.setText(selectedPlanet.containingFile)
 
     def __okayClicked(self) -> None:
         '''Okay button handler. Performs minor error checking and adds trade route to repository'''
         self.__variantName = self.__inputVariantName.text()
         self.__basePlanet = self.__inputBaseName.text()
+        self.__newX = float(self.__inputX.text())
+        self.__newY = float(self.__inputY.text())
+        self.__file = self.__inputFile.text()
 
         if not self.__inputDataIsValid():
-            print("Error! Couldn't find base planet!")
+            print("Error! Variant name or base planet invalid!")
             return
 
         if self.__repository.planetExists(self.__variantName):
-            print("Error! Planet already exists!")
+            print("Error! Planet name already exists!")
+            return
+
+        if self.__file == "":
+            print("Error! No file name given!")
             return
 
         self.__result = DialogResult.Ok
